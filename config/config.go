3951f387f9fa4bf1bd09f3b4248ec655726b5799
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-07-15 13:15:33 +0500

    initial commit

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	ORDER_PORT  string
+	DB_HOST     string
+	DB_PORT     string
+	DB_USER     string
+	DB_NAME     string
+	DB_PASSWORD string
+}
+
+func Load() *Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("error while loading .env file: %v", err)
+	}
+
+	return &Config{
+		ORDER_PORT:  cast.ToString(coalesce("ORDER_PORT", ":50052")),
+		DB_HOST:     cast.ToString(coalesce("DB_HOST", "localhost")),
+		DB_PORT:     cast.ToString(coalesce("DB_PORT", "5432")),
+		DB_USER:     cast.ToString(coalesce("DB_USER", "postgres")),
+		DB_NAME:     cast.ToString(coalesce("DB_NAME", "order_service")),
+		DB_PASSWORD: cast.ToString(coalesce("DB_PASSWORD", "password")),
+	}
+}
+
+func coalesce(key string, value interface{}) interface{} {
+	val, exist := os.LookupEnv(key)
+	if exist {
+		return val
+	}
+	return value
+}
